3e5017b649303a48121298cb3477591d0276a335
Fidel Soto <fidelsotostem@gmail.com>
2024-07-15 00:13:23 -0600

    Refactor of Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,19 +1,71 @@
-﻿namespace Day1;
+﻿using System.IO;
+using System.Numerics;
 
-class Digit {
-    public int Value {get; set; } = 0;
-    public bool IsSet { get; set; } = false;
-}
+namespace Day1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int calibrationValueSum = 0;
+        if(args.Length != 2)
+        {
+            Console.WriteLine("Provide 2 arguments ([partNumber] [fileName]):\n\tPart1: 1 [fileName]\n\tPart2: 2 [fileName]");
+            return;
+        }
 
-        Digit firstDigit = new Digit();
-        Digit secondDigit = new Digit();
 
+        switch(args[0])
+        {
+            case "1":
+                Part1(args[1]);
+                break;
+            case "2":
+                Part2(args[1]);
+                break;
+            default: 
+                Console.WriteLine("Provide 2 arguments ([partNumber] [fileName]):\n\tPart1: 1 [fileName]\n\tPart2: 2 [fileName]");
+                return;
+        }
+    }
+
+
+    static void Part1(string fileName)
+    {
+        StreamReader? streamReader;
+        if(!OpenFile(out streamReader,fileName))
+        {
+            return;
+        }
+
+        int sum = 0;
+        string currentNumber = "";
+        string? line = streamReader?.ReadLine();
+
+        while (line != null)
+        {
+
+            for(int i = 0; i < line.Length; i++)
+            {
+                // Ascii 0-9 == 48-57
+                if(line[i] >= 48 && line[i] <= 57)
+                {
+                    currentNumber += line[i];
+                }
+            }
+
+            currentNumber = "" + currentNumber[0] + currentNumber[currentNumber.Length - 1];
+
+            sum += int.Parse(currentNumber);
+
+            currentNumber = "";
+            line = streamReader?.ReadLine();
+        }
+
+        Console.WriteLine("Sum: " + sum);
+    }
+
+    static void Part2(string fileName)
+    {
         Dictionary<string,int> numberDict = new Dictionary<string, int>(){
             {"zero",0},     {"0",0},
             {"one",1},      {"1",1},
@@ -21,63 +73,68 @@ class Program
             {"three",3},    {"3",3},
             {"four",4},     {"4",4},
             {"five",5},     {"5",5},
-            {"6",6},        {"six",6},
-            {"7",7},        {"seven",7},
-            {"8",8},        {"eight",8},
-            {"9",9},        {"nine",9}
+            {"six",6},      {"6",6},
+            {"seven",7},    {"7",7},
+            {"eight",8},    {"8",8},
+            {"nine",9},     {"9",9}
         };
-        
-        StreamReader streamReader = new StreamReader("input.txt");
-        char[]? line;
-        string currentEval = "";
-        int stringRemainingLength = 0;
-        int currentLine = 1;
-        string lineString = "";
 
+        StreamReader? streamReader;
+        if (!OpenFile(out streamReader, fileName))
+        {
+            return;
+        }
 
-        while ((line = streamReader.ReadLine()?.ToCharArray()) != null) {
+        string? line = streamReader?.ReadLine();
+        string currentNumber = "";
+        string currentKey;
+        int sum = 0;;
+        int stringRemainingLength;
+        while(line != null)
+        {
+            stringRemainingLength = line.Length;
 
-            lineString = new string(line);
-            stringRemainingLength = lineString.Length;
-            currentEval = "";
-
-
-            // Left to right for second digit. Eval indiscriminately for second digit.
-            for (int i = 0; i < lineString.Length; i++){
-
-                for (int j = 1; j <= stringRemainingLength; j++){
+            for(int i = 0; i < line.Length; i++)
+            {
+                for (int j = 1; j <= stringRemainingLength; j++)
+                {
                     
-                    currentEval = lineString.Substring(i,j);
+                    currentKey = line.Substring(i,j);
 
-                    if (numberDict.ContainsKey(currentEval)){
-                        Console.WriteLine("Line: " + currentLine + "\t2nd Digit match. currentEval = " + currentEval);
-                        if (!firstDigit.IsSet){
-                            firstDigit.Value = numberDict[currentEval];
-                            firstDigit.IsSet = true;
-                        }
-                        secondDigit.Value = numberDict[currentEval];
-                        secondDigit.IsSet = true;
+                    if (numberDict.ContainsKey(currentKey)){
+                        currentNumber += numberDict[currentKey];
                         break;
                     }
 
                 }
 
                 stringRemainingLength--;
-
             }
 
-            calibrationValueSum += int.Parse($"{firstDigit.Value}{secondDigit.Value}");
-            Console.WriteLine($"Line Result: {firstDigit.Value}{secondDigit.Value}");
-
-            firstDigit.IsSet = false;
-            firstDigit.Value = 0;
-            secondDigit.IsSet = false;
-            secondDigit.Value = 0;
-            currentLine++;
-
+            currentNumber = "" + currentNumber[0] + currentNumber[currentNumber.Length - 1];
+            sum += int.Parse(currentNumber);
+            
+            currentNumber = "";
+            line = streamReader?.ReadLine();
         }
 
-        Console.WriteLine(calibrationValueSum);
+        Console.WriteLine(sum);
+
+    }
+
+    static bool OpenFile(out StreamReader? streamReader,string fileName)
+    {
+        try
+        {
+            streamReader = new(fileName);
+            return true;
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("Couldn't load file");
+            streamReader = null;
+            return false;
+        }
 
     }
 }
