8060bf642bc29e7ca215e86a5ff196e31aeea6f1
Fidel Soto <fidelsotostem@gmail.com>
2024-08-09 12:32:07 -0600

    Day 7 Part 2. Pretty fast, but could use some cleaning up though

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -150,7 +150,171 @@ class Program
 
     static void Part2(string fileName)
     {
-        
+        (StreamReader? streamReader, Exception? exception) = OpenFile(fileName);
+        if (exception != null)
+        {
+            Console.WriteLine(GetFullError(exception));
+            return;
+        }
+
+        Dictionary<char,int> cards = new()
+        {
+            {'J',1},{'2',2}, {'3',3}, {'4',4}, {'5',5},
+            {'6',6}, {'7',7}, {'8',8}, {'9',9},
+            {'T',10},{'Q',11}, {'K',12},{'A',13}
+        };
+
+        List<List<Hand>> handsGrouped =
+            streamReader!.ReadToEnd().Split('\n')
+            .Select(line => {
+                string[] elements = line.Split(' ');
+                if(elements[0].Length < 5)
+                {
+                    throw new Exception("ahhh");
+                }
+                return new Hand(elements[0],int.Parse(elements[1]));
+            })
+            .ToList()
+            .GroupBy(hand => {
+                int matchNum;
+                int numPairs = 0;
+                string originalHand = hand.Cards;
+
+                /*
+                    | 0: High Cards | 1: One Pair
+                    | 2: Two Pair   | 3; Three of a kind 
+                    | 4: Full House | 5: Four of a kind
+                    | 6: Five of a kind
+                */
+                foreach(char c in originalHand)
+                {
+                    matchNum = originalHand.Where(handChar => c == handChar).Count();
+                    switch(matchNum)
+                    {
+                        case 5:
+                            hand.HandType = 6;
+                            return hand.HandType;
+                        case 4:
+                            hand.HandType = 5;
+                            if (c == 'J' || hand.Cards.Contains('J')) { hand.HandType = 6; } // Promote to 5 of a kind
+                            return hand.HandType;
+                        case 3:
+                            if (c == 'J')
+                            {
+                                hand.HandType = 5; // Promote to 4 of a kind
+                                if (hand.Cards.Where(dc => dc != 'J').Distinct().Count() == 1)
+                                {
+                                    hand.HandType = 6;
+                                }
+                            }
+                            else if(hand.Cards.Where(dc => dc != c).Distinct().Count() == 2)
+                            {
+                                hand.HandType = 3;
+                                if(hand.Cards.Where(dc => dc == 'J').Count() == 1)
+                                {
+                                    hand.HandType = 5;
+                                }
+                            }
+                            else
+                            {
+                                hand.HandType = 4;
+                                if(hand.Cards.Where(dc => dc == 'J').Count() == 2)
+                                {
+                                    hand.HandType = 6;
+                                }
+                            }
+                            return hand.HandType;
+                        case 2:
+                            numPairs++;
+                            originalHand = originalHand.Replace(c.ToString(),"");
+                            break;
+                    }
+                }
+
+                switch(numPairs)
+                {
+                    case 2:
+                        if (hand.Cards.Where(c => c == 'J').Count() == 2)
+                        {
+                            // One of the Pairs was JJ. We can replace those
+                            // and promote to 4 of a kind, which will combine with
+                            // the other pair of characters
+                            hand.HandType = 5;
+                        }
+                        else if (hand.Cards.Where(c => c == 'J').Count() == 1)
+                        {
+                            // TTAAJ => TTAAA or TTAAT. Full house in any case
+                            hand.HandType = 4;
+                        }
+                        else
+                        {
+                            hand.HandType = numPairs;
+                        }
+                        break;
+                    case 1:
+                        if(hand.Cards.Where(c => c == 'J').Count() == 2 || hand.Cards.Where(c => c == 'J').Count() == 1)
+                        {
+                            hand.HandType = 3;
+                        }
+                        else
+                        {
+                            hand.HandType = numPairs;
+                        }
+                        break;
+                    case 0:
+                        if(hand.Cards.Contains('J'))
+                        {
+                            numPairs++;
+                        }
+                        hand.HandType = numPairs;
+                        break;
+                    default:
+                        throw new Exception("aaaaahhh");
+                }
+
+                return hand.HandType;
+
+            })
+            .OrderBy(group => group.Key)
+            .Select(group => group.ToList()).ToList();
+
+        int rank = 1;
+        long totalWinnings = 0;
+        Hand temp;
+        foreach(List<Hand> handGroup in handsGrouped)
+        {
+            for(int pass = 0; pass < handGroup.Count - 1; pass++)
+            {
+                for(int hand = 0; hand < handGroup.Count - pass - 1; hand++)
+                {
+                    for(int character = 0; character < handGroup[hand].Cards.Length; character++)
+                    {
+                        if(cards[handGroup[hand].Cards[character]] > cards[handGroup[hand + 1].Cards[character]])
+                        {
+                            temp = handGroup[hand];
+                            handGroup[hand] = handGroup[hand + 1];
+                            handGroup[hand + 1] = temp;
+                            break;
+                        }
+                        else if(cards[handGroup[hand].Cards[character]] < cards[handGroup[hand + 1].Cards[character]])
+                        {
+                            break;
+                        }
+                    }
+                }           
+            }
+
+            for(int hand = 0; hand < handGroup.Count; hand++)
+            {
+                handGroup[hand].Rank = rank++;
+                totalWinnings += handGroup[hand].Rank * handGroup[hand].Bid;
+            }
+        }
+
+        Console.WriteLine("Total Winnings: " + totalWinnings);
+
+        return;
+
     }
 
     static Tuple<StreamReader?,Exception?> OpenFile(string fileName)
